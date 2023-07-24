f0e79694724a664b680238aa5b1bc77c263054e9
Narendra5305 <narendrasinghsitu4@gmail.com>
2023-07-24 22:35:07 +0530

    project completed

---
+++ project3.py
@@ -0,0 +1,20 @@
+import instaloader
+from pytube import YouTube
+def video_from_youtube(url):
+    yt=YouTube(url)
+    so=yt.streams.filter(resolution="720p",file_extension="mp4").first()
+    so.download()
+def instagram():
+    insta = instaloader.Instaloader()
+    user = input("Enter your instagram username: ")
+    insta.download_profile(user, profile_pic_only=True)
+    profile = instaloader.Profile.from_username(insta.context, user)
+    posts = list(profile.get_posts())
+    latestPost = posts[0]
+    insta.download_post(latestPost, target=profile.username)
+choice = input("Enter 1 for instagram operation and 2 for youtube: ")
+if choice == '1':
+    instagram()
+else:
+    video_url = input("Enter youtube url")
+    video_from_youtube(video_url)
